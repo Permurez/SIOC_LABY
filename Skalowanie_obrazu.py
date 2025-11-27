02bd771c4faa166043f3ad9090edb90064c2410b
Kamil <kamil.rachwalik@o2.pl>
2025-11-27 14:34:36 +0100

    Skalowanie_obrazu zrobione

--- Skalowanie_obrazu.py
+++ Skalowanie_obrazu.py
@@ -1,13 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+#zmiana jader uzywanych przy skalowaniu
+#wiersz
+aaa=1
+#kolumna
+bbb=2
+#ile razy zmniejszamy i powiekszamy
+factor=1
 img = plt.imread('kamien.jpg')
 if len(img.shape) == 3:  
     img = np.dot(img[..., :3], [0.299, 0.587, 0.114])
 # RGB to Grey
 Grey_Image = img.astype(np.float64) 
 H, W =Grey_Image.shape
-factor = 2
+
 def MSE_(image1, image2):
     # Dopasowanie rozmiaru: bierzemy wspólny fragment
     H = min(image1.shape[0], image2.shape[0])
@@ -31,15 +37,26 @@ def Reduce_Image (img, factor):
             conv[i, j] = np.sum(patch * kernel)
     reduced = conv[::factor, ::factor]
     return reduced
-
-def Enlarge_Image(image, factor):
+#Reduce_Image gotowe
+def Enlarge_Image(image, factor, kernel_func1, kernel_func2):
     H, W = image.shape
-    enlarged = np.zeros((H * factor, W * factor))
-    
+    H_new, W_new = H * factor, W * factor
+    enlarged = np.zeros((H_new, W_new))
+
+    x_org = np.arange(W)
+    y_org = np.arange(H)
+    x_new = np.linspace(0, W - 1, W_new)
+    y_new = np.linspace(0, H - 1, H_new)
+
+#wierszowa
+    temp_image = np.zeros((H, W_new))
     for i in range(H):
-        for j in range(W):
-            enlarged[i*factor:(i+1)*factor, j*factor:(j+1)*factor] = image[i, j]
-    
+        temp_image[i, :] = interpolacja(x_org, image[i, :], x_new, kernel_func1)
+
+#kolumnowa
+    for j in range(W_new):
+        enlarged[:, j] = interpolacja(y_org, temp_image[:, j], y_new, kernel_func2)
+        
     return enlarged
 #z poprzedniego ćwicznenia
 def h1(t):
@@ -63,9 +80,9 @@ jadra = {1: h1, 2: h2, 3: h3, 4: h4}
 
 
 reduced_image = Reduce_Image(Grey_Image, factor)
-reduced_enlarged_image = Enlarge_Image(reduced_image, factor)
+reduced_enlarged_image = Enlarge_Image(reduced_image, factor, jadra[aaa], jadra[bbb])  
 
-# Oblicz MSE między oryginalnym obrazem a powiększonym obrazem
+# Oblicza MSE między oryginalnym obrazem a powiększonym obrazem
 mse_value = MSE_(Grey_Image, reduced_enlarged_image)
 
 plt.figure(figsize=(18, 5))
@@ -81,7 +98,7 @@ plt.axis('off')
 
 plt.subplot(1, 3, 3)
 plt.imshow(reduced_enlarged_image, cmap='gray')
-plt.title(f'Enlarged Image (MSE={mse_value:.2f})')
+plt.title(f'Enlarged Image (h{aaa}/h{bbb}, MSE={mse_value:.2f})')
 plt.axis('off')
 
 plt.tight_layout()
